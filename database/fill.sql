3672840e442333306c741491a1cfbce5e0a826f5
Leiki á Mýrini <1065489@ucn.dk>
2018-11-20 10:04:32 +0100

    insert into done(need lasse check)

--- database/fill.sql
+++ database/fill.sql
@@ -56,8 +56,6 @@ INSERT INTO UserBase (type, first_name, last_name, email, password) VALUES ('pat
 INSERT INTO UserBase (type, first_name, last_name, email, password) VALUES ('patient', 'Amalie', 'Olsen', 'aolsen@hotmail.com');
 INSERT INTO UserBase (type, first_name, last_name, email, password) VALUES ('patient', 'Fie', 'Nielsen', 'nielsenfie@hotmail.com');
 
-INSERT INTO Physiotherapist (user_id, location_id, birth_date, sex, height, weight) VALUES (1, 1, 1973-02-25, 'male', 181, 74);
-INSERT INTO Physiotherapist (user_id, location_id, birth_date, sex, height, weight) VALUES (2, 2, 1965-08-11, 'male', 184, 85);
 INSERT INTO Patient (user_id, location_id, birth_date, sex, height, weight) VALUES (3, 3, 1957-03-05, 'female', 165, 60);
 INSERT INTO Patient (user_id, location_id, birth_date, sex, height, weight) VALUES (4, 4, 1943-11-08, 'male', 175, 91);
 INSERT INTO Patient (user_id, location_id, birth_date, sex, height, weight) VALUES (5, 5, 1961-09-18, 'male', 190, 110);
@@ -73,3 +71,68 @@ INSERT INTO Dizziness (user_id, level, note) VALUES (5, 7, '');
 INSERT INTO Dizziness (user_id, level, note) VALUES (6, 7, '');
 INSERT INTO Dizziness (user_id, level, note) VALUES (7, 8, 'Rigtig svimmel');
 INSERT INTO Dizziness (user_id, level, note) VALUES (7, 9, 'Værre i dag');
+
+INSERT INTO Department(location_id, name, phone) VALUES (1, 'Department Navn 1', 59285722);
+INSERT INTO Department(location_id, name, phone) VALUES (1, 'Department Navn 2', 52958354);
+
+INSERT INTO Physiotherapist (user_id, location_id, birth_date, sex, height, weight) VALUES (1, 1, 1973-02-25, 'male', 181, 74);
+INSERT INTO Physiotherapist (user_id, location_id, birth_date, sex, height, weight) VALUES (2, 2, 1965-08-11, 'male', 184, 85);
+
+INSERT INTO Request(physiotherapist_id, patient_id, accepted) VALUES (1, 1, false);
+INSERT INTO Request(physiotherapist_id, patient_id, accepted) VALUES (1, 2, true);
+INSERT INTO Request(physiotherapist_id, patient_id, accepted) VALUES (2, 1, false);
+INSERT INTO Request(physiotherapist_id, patient_id, accepted) VALUES (2, 2, false);
+INSERT INTO Request(physiotherapist_id, patient_id, accepted) VALUES (2, 3, true);
+
+INSERT INTO Period(request_id) VALUES (1);
+INSERT INTO Period(request_id) VALUES (2);
+INSERT INTO Period(request_id) VALUES (3);
+INSERT INTO Period(request_id) VALUES (4);
+INSERT INTO Period(request_id) VALUES (5);
+
+INSERT INTO JournalEntry(patient_id, note) VALUES (1, ''); //what write here
+INSERT INTO JournalEntry(patient_id, note) VALUES (1, '');
+INSERT INTO JournalEntry(patient_id, note) VALUES (1, '');
+INSERT INTO JournalEntry(patient_id, note) VALUES (1, '');
+INSERT INTO JournalEntry(patient_id, note) VALUES (2, '');
+INSERT INTO JournalEntry(patient_id, note) VALUES (2, '');
+INSERT INTO JournalEntry(patient_id, note) VALUES (2, '');
+
+INSERT INTO StepCount(patient_id, count, date) VALUES (1, 400);
+INSERT INTO StepCount(patient_id, count, date) VALUES (1, 809);
+INSERT INTO StepCount(patient_id, count, date) VALUES (2, 643);
+INSERT INTO StepCount(patient_id, count, date) VALUES (2, 728);
+INSERT INTO StepCount(patient_id, count, date) VALUES (3, 570);
+
+INSERT INTO Exercise(author_id, name, description) VALUES (1, 'Lunges');
+INSERT INTO Exercise(author_id, name, description) VALUES (1, 'Squats');
+INSERT INTO Exercise(author_id, name, description) VALUES (2, 'Raise Shoulders');
+INSERT INTO Exercise(author_id, name, description) VALUES (2, 'Turn Head');
+
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (1, 1, 1, true);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (1, 2, 2, true);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (2, 3, 2, true);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (1, 4, 3, false);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (2, 5, 4, false);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (2, 6, 5, false);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (3, 7, 6, false);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (2, 8, 7, true);
+INSERT INTO ExerciseFeedback(exercise_id, dizziness_id, patient_id, dizziness_given) VALUES (3, 9, 7, true);
+
+INSERT INTO ExerciseFavorite(exercise_id, patient_id) VALUES (4, 1);
+INSERT INTO ExerciseFavorite(exercise_id, patient_id) VALUES (2, 1);
+INSERT INTO ExerciseFavorite(exercise_id, patient_id) VALUES (2, 2);
+INSERT INTO ExerciseFavorite(exercise_id, patient_id) VALUES (3, 3);
+
+INSERT INTO CustomExercise(author_id, name, description) VALUES (1, 'Touch toes', 'Bend down and try to touch your toes');
+INSERT INTO CustomExercise(author_id, name, description) VALUES (1, 'Look at ceiling and floor', 'First look at the ceiling and then at the floor and repeat');
+
+INSERT INTO CustomExercisePatient(custom_exercise_id, patient_id) VALUES (1, 1);
+INSERT INTO CustomExercisePatient(custom_exercise_id, patient_id) VALUES (1, 1);
+INSERT INTO CustomExercisePatient(custom_exercise_id, patient_id) VALUES (2, 2);
+INSERT INTO CustomExercisePatient(custom_exercise_id, patient_id) VALUES (2, 3);
+
+INSERT INTO Recommendation(physiotherapist_id, exercise_id, patient_id, note) VALUES (1, 1, 1, 'This exercise will be good for you');   
+INSERT INTO Recommendation(physiotherapist_id, exercise_id, patient_id, note) VALUES (1, 2, 1, 'This should fit for you');
+INSERT INTO Recommendation(physiotherapist_id, exercise_id, patient_id, note) VALUES (2, 3, 2, 'My custom exercise for you');
+INSERT INTO Recommendation(physiotherapist_id, exercise_id, patient_id, note) VALUES (2, 4, 3, 'This should do the trick');
