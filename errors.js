b321b13f135934a8cd199d60d9a00164bac344cd
Lasse Fisker Olesen <fiskero94@gmail.com>
2018-12-02 20:35:45 +0100

    Auth user checks

--- errors.js
+++ errors.js
@@ -1,6 +1,9 @@
 module.exports = {
-    validation: { code: "40", message:"Validatin error." },
-    invalidEmailOrPassword: { code: "41", message: "Invalid email or password." },
-    userAlreadyRegistered: { code: "42", message: "User already registered." },
-    internalServerError: { code: "50", message: "Internal server error." }
+    validation: { code: "40001", message:"Validatin error." },
+    invalidEmailOrPassword: { code: "40002", message: "Invalid email or password." },
+    userAlreadyRegistered: { code: "40003", message: "User already registered." },
+    noToken: { code: "40004", message: "Access denied. No token provided." },
+    invalidToken: { code: "40005", message: "Invalid token." },
+    userNotFound: { code: "40401", message: "The user no longer exists." },
+    internalServerError: { code: "50001", message: "Internal server error." }
 }
