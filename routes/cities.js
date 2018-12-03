403c5f6d45091be3e3cc4a9be9b39672fde4897a
Lasse Fisker Olesen <fiskero94@gmail.com>
2018-12-03 07:44:29 +0100

    fixes to cities countries

--- routes/cities.js
+++ routes/cities.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 const pool = require('../database/pool');
 const errors = require('../errors.js');
 
 router.get('/', getAllCities);
-router.get('/:id', getCity);
+router.get('/:countrycode/:zipcode', getCity);
 
 async function getAllCities(request, response) {
     try {
@@ -16,11 +17,19 @@ async function getAllCities(request, response) {
 }
 
 async function getCity(request, response) {
-    const id = parseInt(request.params.id);
-    if (isNaN(id)) return response.status(400).send(errors.urlParameterNumber);
+    const result = validate(request);
+    if (result.error) {
+        return response.status(400).send({ 
+            code: errors.validation.code, 
+            message: result.error.details[0].message 
+        });
+    }
 
     try {
-        const selected = await pool.query('SELECT zip_code, country_code, name FROM City WHERE id = $1', [id]);
+        const selected = await pool.query(
+            'SELECT zip_code, country_code, name FROM City WHERE country_code = $1 AND zip_code = $2', 
+            [request.params.countrycode, request.params.zipcode]
+        );
 
         if (selected.rows.length !== 1)
             return response.status(404).send(errors.elementNotFound);
@@ -29,6 +38,13 @@ async function getCity(request, response) {
     } catch(error) {
         return response.status(500).send(errors.internalServerError);
     }
+}
+
+function validate(request) {
+    return Joi.validate(request.params, {
+        countrycode: Joi.string().required(),
+        zipcode: Joi.string().required()
+    });
 }
 
 module.exports = router;
--- routes/countries.js
+++ routes/countries.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 const pool = require('../database/pool');
 const errors = require('../errors.js');
 
 router.get('/', getAllCountries);
-router.get('/:id', getCountry);
+router.get('/:countrycode', getCountry);
 
 async function getAllCountries(request, response) {
     try {
@@ -16,19 +17,35 @@ async function getAllCountries(request, response) {
 }
 
 async function getCountry(request, response) {
-    const id = parseInt(request.params.id);
-    if (isNaN(id)) return response.status(400).send(errors.urlParameterNumber);
+    const result = validate(request);
+    if (result.error) {
+        return response.status(400).send({ 
+            code: errors.validation.code, 
+            message: result.error.details[0].message 
+        });
+    }
 
     try {
-        const selected = await pool.query('SELECT code, name FROM Country WHERE id = $1', [id]);
+        const selected = await pool.query(
+            'SELECT code, name FROM Country WHERE code = $1', 
+            [request.params.countrycode]
+        );
 
         if (selected.rows.length !== 1)
             return response.status(404).send(errors.elementNotFound);
 
         return response.send(selected.rows[0]);
     } catch(error) {
+        console.log(error);
+        
         return response.status(500).send(errors.internalServerError);
     }
+}
+
+function validate(request) {
+    return Joi.validate(request.params, {
+        countrycode: Joi.string().required()
+    });
 }
 
 module.exports = router;
