1b898cef16965147e52c53b952e33ed29f011b1c
MathiasWael <mathiaswael@hotmail.dk>
2018-12-14 13:15:26 +0100

    stepcountstuff

--- routes/stepcounts.js
+++ routes/stepcounts.js
@@ -4,8 +4,14 @@ const pool = require('../database/pool');
 const auth = require('../middleware/auth');
 const errors = require('../errors.js');
 const getpatients = require('../middleware/getpatients');
+const onlypatient = require('../middleware/onlypatient');
+const BaseJoi = require('joi');
+const Extension = require('joi-date-extensions');
+const Joi = BaseJoi.extend(Extension);
 
-router.get('/:userid/stepcounts/', [auth, getpatients], getAllStepCounts);
+router.get('/:userid/stepcounts', [auth, getpatients], getAllStepCounts);
+router.post('/:userid/stepcounts', [auth, onlypatient], createStepCount);
+router.put('/:userid/stepcounts/:id', [auth, onlypatient], updateStepCount);
 
 async function getAllStepCounts(request, response) {
     const userid = parseInt(request.params.userid);
@@ -27,6 +33,69 @@ async function getAllStepCounts(request, response) {
     } catch(error) {
         return response.status(500).send(errors.internalServerError);
     }
+}
+
+async function createStepCount(request, response) {
+    const userid = parseInt(request.params.userid);
+    if (isNaN(userid)) return response.status(400).send(errors.urlParameterNumber);
+
+    if (request.user.sub != userid) 
+        return response.status(403).send(errors.accessDenied);
+
+    const result = validate(request);
+    if (result.error) {
+        return response.status(400).send({
+            code: errors.validation.code,
+            message: result.error.details[0].message
+        });
+    }
+    
+    try {
+        const created = await pool.query(`
+            INSERT INTO stepcount (patient_id, count, date) VALUES ($1, $2, $3) RETURNING *`, 
+            [request.body.patient_id, request.body.count, request.body.date]
+        );
+
+        return response.send(created.rows[0]);
+    } catch(error) {
+        return response.status(500).send(errors.internalServerError);
+    }
+}
+
+async function updateStepCount(request, response) {
+    const userid = parseInt(request.params.userid);
+    const stepid = parseInt(request.params.id);
+    if (isNaN(userid) || isNaN(stepid)) return response.status(400).send(errors.urlParameterNumber);
+    
+    if (request.user.sub != userid) 
+        return response.status(403).send(errors.accessDenied);
+
+    const result = validate(request);
+    if (result.error) {
+        return response.status(400).send({
+            code: errors.validation.code,
+            message: result.error.details[0].message
+        });
+    }
+    
+    try {
+        const updated = await pool.query(`
+            UPDATE stepcount SET count = $1 WHERE id = $2 RETURNING *`, 
+            [request.body.count, stepid]
+        );
+
+        return response.send(updated.rows[0]);
+    } catch(error) {
+        return response.status(500).send(errors.internalServerError);
+    }
+}
+
+function validate(request) {
+    return Joi.validate(request.body, {
+        patient_id: Joi.number().required(),
+        count: Joi.number().required(),
+        date: Joi.string().required(),
+    });
 }
 
 module.exports = router;
